9b633ddb33f33f26590a68fc6cb41c66a8bb1fc7
arjun <arjun.vmaniyara@gmail.com>
2023-09-27 11:56:27 +0530

    jwt

--- Server/src/Framework/Service/user/userServiceImp.js
+++ Server/src/Framework/Service/user/userServiceImp.js
@@ -5,7 +5,7 @@ import config from '../../../../config.js';
 const authServiceImp=()=>{
     
 
-    const generateAccessToken=(user)=>jwt.sign({user},config.ACESS_TOKEN_SCERET,{expiresIn:'25m'});
+    const generateAccessToken=(user)=>jwt.sign({user},config.ACESS_TOKEN_SCERET,{expiresIn:'5m'});
     const verifyAccessToken = (token) => jwt.verify(token, config.ACESS_TOKEN_SCERET);
     
 
